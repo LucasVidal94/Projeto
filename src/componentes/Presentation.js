47ad412157d59206db3db16711fe97246d1eadea
Lucas Vidal <vidal.lucas94@gmail.com>
2024-02-07 12:55:54 -0300

    versao-2

--- src/componentes/Presentation.js
+++ src/componentes/Presentation.js
@@ -1,13 +1,58 @@
 import styles from './Presentation.module.css'
 import ButtonA from './elements/ButtonA'
 import ButtonB from './elements/ButtonB'
+import{useEffect, useState} from 'react'
 
 
 function Presentation (){
+const [text, setText]= useState ('');
+const toRotate = ['Lucas Vidal!', 'estudante de Desenvolvimento' ];
+const [loop, setLoop] = useState (0);
+const [isDeleting, setIsDeleting]= useState (false);
+const period = 100;
+const [delta, setDelta]= useState (100);
+
+useEffect (()=>{
+  let ticker = setInterval (()=>{
+
+    toType()
+  }, delta)
+
+
+  return ()=>{clearInterval(ticker)}
+
+}, [text])
+
+const toType =() => {
+let i  = loop % toRotate.length;
+let fullText = toRotate [i];
+let updateText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+setText (updateText);
+
+
+if(!isDeleting && updateText === fullText){
+
+    setIsDeleting(true);
+    setDelta (period);
+} else if (isDeleting && updateText === '') {
+    setIsDeleting(false);
+    setDelta(period);
+    setLoop (loop+1)
+
+
+}
+
+}
+
+
+
+
+
     return (
      <div className={styles.presentation} id='Presentation'> 
      <h4><strong> Bem-vindo ao meu Portfólio </strong></h4>
-     <h1> Olá, eu sou Lucas Vidal!</h1>
+     <h1> Olá, eu sou {text}</h1>
      <p>
       Sou estudante de Engenharia da Computação com conclusão em 2025 <br/>
       e busco transição de carreira para oportunidades de estágio na área de tecnologia.<br/>
